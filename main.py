fac17abad7f2ed2bde577939852bed9237e25054
itomicic <ivan.tomicic@outlook.com>
2023-11-26 20:50:44 +0100

    generate review templates

--- main.py
+++ main.py
@@ -30,10 +30,6 @@ list_of_models = [
     {
         "model": "TheBloke/zephyr-7B-beta-GGUF",
         "model_file": "zephyr-7b-beta.Q5_K_M.gguf"
-    },
-    {
-        "model": "TheBloke/zephyr-7B-beta-GGUF",
-        "model_file": "zephyr-7b-beta.Q5_K_S.gguf"
     }
 ]
 
@@ -59,13 +55,26 @@ db = FAISS.load_local("faiss", embeddings)
 # prepare a version of the llm pre-loaded with the local content
 retriever = db.as_retriever(search_kwargs={'k': 2})"""
 
-human_eval_metrics = {
-    "fluency": 0,
-    "coherence": 0,
-    "relevance": 0,
-    "context_understanding": 0,
-    "overall_quality": 0
-}
+def get_human_eval_metric(reviewer_from, reviewer_to):
+    human_eval_metrics = {
+        "fluency": {
+            f"reviewer_{i}": "_" for i in range(reviewer_from, reviewer_to + 1)
+        },
+        "coherence": {
+            f"reviewer_{i}": "_" for i in range(reviewer_from, reviewer_to + 1)
+        },
+        "relevance": {
+            f"reviewer_{i}": "_" for i in range(reviewer_from, reviewer_to + 1)
+        },
+        "context_understanding": {
+            f"reviewer_{i}": "_" for i in range(reviewer_from, reviewer_to + 1)
+        },
+        "overall_quality": {
+            f"reviewer_{i}": "_" for i in range(reviewer_from, reviewer_to + 1)
+        },
+    }
+    return human_eval_metrics
+
 
 eval_dict = {
     "bleu_score": -100_000,
@@ -75,16 +84,13 @@ eval_dict = {
         "f1": -100_000
     },
     "diversity": -100_000,
-    "human_eval": {
-        f"reviewer_{i}": human_eval_metrics for i in range(1, 16)
-    },
 }
 
 with open("test_results_new/questions.json", "r", encoding='utf-8') as f:
     questions = json.load(f)
 cnt_ = 1
 
-for model in list_of_models:
+for i, model in enumerate(list_of_models):
     # create a file where we will store models answers
     answer_file = open(f"test_results_new/answers/{model['model_file']}.json", "w+", encoding='utf-8')
     print(answer_file)
@@ -105,8 +111,9 @@ for model in list_of_models:
         question_number = list(question_dict.keys())[0]
         question = question_dict[question_number]
         time_start = time.time()
-        output = "dssdsd" #qa.run(question_text)
+        output = "<answer>" #qa.run(question_text)
         time_end = time.time()
+        eval_dict["human_eval"] = get_human_eval_metric((i*3) + 1, (i+1)*3)
         answers.append({
             "question_number": question_number,
             "question": question,
