b0293e388ce03e4f04db690159a2635821c54ba4
itomicic <ivan.tomicic@outlook.com>
2023-11-28 11:55:28 +0100

    generate review templates

--- generate_review_templates.py
+++ generate_review_templates.py
@@ -11,7 +11,7 @@ set_5 = []
 def fill_set(file_path, set_):
     with open(file_path, "r", encoding='utf-8') as f:
         all_answers = json.load(f)
-        for answer in all_answers[0:90]:
+        for answer in all_answers[0:92]:
             ordered_answer = OrderedDict([
                 ("question", answer["question"]),
                 ("answer", answer["answer"]),
@@ -31,6 +31,12 @@ fill_set("test_results_new/answers/mistral-7b-instruct-v0.1.Q5_K_M.gguf.json", s
 fill_set("test_results_new/answers/mistral-7b-openorca.Q4_0.gguf.json", set_3)
 fill_set("test_results_new/answers/zephyr-7b-alpha.Q5_K_M.gguf.json", set_4)
 fill_set("test_results_new/answers/zephyr-7b-beta.Q5_K_M.gguf.json", set_5)
+
+# reviewer_1-3 = llama-2-7b-chat.Q5_K_M.gguf.json
+# reviewer_4-6 = mistral-7b-instruct-v0.1.Q5_K_M.gguf.json
+# reviewer_7-9 = mistral-7b-openorca.Q4_0.gguf.json
+# reviewer_10-12 = zephyr-7b-alpha.Q5_K_M.gguf.json
+# reviewer_13-15 = zephyr-7b-beta.Q5_K_M.gguf.json
 
 for i in range(1, 16):
     set_ = globals()[f'set_{(i-1)//3 + 1}']
