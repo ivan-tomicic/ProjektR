fa1b6e13c807b34fec65de935e63624b2c0bf40b
itomicic <ivan.tomicic@outlook.com>
2023-12-04 07:29:24 +0100

    minor stuff

--- generate_review_templates.py
+++ generate_review_templates.py
@@ -32,12 +32,6 @@ fill_set("test_results_new/answers/mistral-7b-openorca.Q4_0.gguf.json", set_3)
 fill_set("test_results_new/answers/zephyr-7b-alpha.Q5_K_M.gguf.json", set_4)
 fill_set("test_results_new/answers/zephyr-7b-beta.Q5_K_M.gguf.json", set_5)
 
-# reviewer_1-3 = llama-2-7b-chat.Q5_K_M.gguf.json
-# reviewer_4-6 = mistral-7b-instruct-v0.1.Q5_K_M.gguf.json
-# reviewer_7-9 = mistral-7b-openorca.Q4_0.gguf.json
-# reviewer_10-12 = zephyr-7b-alpha.Q5_K_M.gguf.json
-# reviewer_13-15 = zephyr-7b-beta.Q5_K_M.gguf.json
-
 for i in range(1, 16):
     set_ = globals()[f'set_{(i-1)//3 + 1}']
 
